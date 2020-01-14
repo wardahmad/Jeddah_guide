29dff1904af155cc1326f23ae10b2fdb3d2fe52f
Doaa Turkistani <doaa.turkistani@hotmail.com>
2020-01-14 11:39:54 +0300

    App, Nav, CSS

--- jeddah_guide/src/App.js
+++ jeddah_guide/src/App.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react';
+import {Card, Button} from 'react-bootstrap/';
+
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,7 +12,7 @@ import Hotels from './Hotels';
 import Places from './Places';
 import Restaurant from './Restaurant';
 import guide from './DB'
-import logo from './logo.svg';
+import NavbarMain from './Navbar'
 
 class App extends Component {
   render() {
@@ -18,25 +20,67 @@ class App extends Component {
     const cafes = guide.filter(obj => obj.listName === 'bestCafes')[0].list;
     const places = guide.filter(obj => obj.listName === 'bestPlaces')[0].list;
     const hotels = guide.filter(obj => obj.listName === 'bestHotels')[0].list;
-    return (
-      <Router>
-        <div className='logo'>
-          <img className='appLogo' src={logo}></img>
-        </div>
-        <div>
-          <nav>
-            <Link to="/">Home</Link>{" "}
-            <Link to="/Restaurant">Restaurants</Link>{" "}
-            <Link to="/cafes">Cafe's</Link>{" "}
-            <Link to="/places">Places</Link>{" "}
-            <Link to="/hotels">Hotels</Link>
-          </nav>
-        </div>
-        <Route path="/Restaurant" component={() => <Restaurant list={restaurant} />} />
-        <Route path="/cafes" component={() => <Cafes list={cafes} />} />
-        <Route path="/places" component={() => <Places list={places} />} />
-        <Route path="/hotels" component={() => <Hotels list={hotels} />} />
-      </Router>
+  return (
+
+      
+  <Router>
+
+   <NavbarMain />
+
+
+   <div className='CardCont'>
+          
+     <div className='Card1'>
+      <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src="https://live.staticflickr.com/65535/49355477498_f6e6edb02b_m.jpg" width="250" height="250" />
+     <Card.Body>
+     <Link to="/Restaurant"><Card.Title>Restaurants</Card.Title></Link>
+     </Card.Body>
+     </Card>
+     </div>
+ 
+
+     <div className='Card2'>
+     <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src="https://live.staticflickr.com/65535/49356142842_5d5f5f1198_m.jpg" width="250" height="250"  />
+     <Card.Body>
+     <Link to="/cafes"><Card.Title>Cafe's</Card.Title></Link>
+     </Card.Body>
+     </Card>
+     </div>
+
+     <div className='Card3'>
+     <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src="https://live.staticflickr.com/65535/49355477153_e4cb13eb6b_m.jpg" width="250" height="250" />
+     <Card.Body>
+     <Link to="/places"><Card.Title>Places</Card.Title></Link>
+     </Card.Body>
+     </Card>
+     </div>
+     
+     <div className='Card4'>
+     <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src="https://live.staticflickr.com/65535/49356142872_e84b35fc65_n.jpg" width="250" height="250" />
+     <Card.Body>
+     <Link to="/hotels"><Card.Title>Hotels</Card.Title></Link>
+     </Card.Body>
+     </Card>
+     </div>
+
+
+
+
+         
+
+
+    </div>
+
+    <Route path="/Restaurant" component={() => <Restaurant list={restaurant} />} />
+    <Route path="/cafes" component={() => <Cafes list={cafes} />} />
+    <Route path="/places" component={() => <Places list={places} />} />
+    <Route path="/hotels" component={() => <Hotels list={hotels} />} />
+    </Router>
+
     )
   }
 }
