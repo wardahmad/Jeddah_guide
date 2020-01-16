64f0e131f6bff986815c4b8fdb2a80d1e902978d
wardahmad <whitelight43@hotmail.com>
2020-01-16 07:46:28 +0300

    AddMapBySarah

--- jeddah_guide/src/Display.js
+++ jeddah_guide/src/Display.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ShowCarousel from './ShowCarousel'
+import Map from './Map'
 
 //Display component handles the display of the details of the item chosen by the user.
 
@@ -9,12 +10,22 @@ class Display extends Component {
         //Return method should render the item's name, description and the display of the set of
         // images related to the currently displayed item, which should be handled by ShowCarousel component.
         return (
-            <div className="contDis">
+            <div className='displayContainer'>
                 <div>
-                    <h3 className="displayH3">{this.props.displayItem.name}</h3>
-                    <ShowCarousel currentItem={this.props.displayItem}></ShowCarousel>
-                    <p className="displayPar">{this.props.displayItem.description}</p>
-                    {console.log(this.props.restaurant)}
+                    <div className="contChild">
+                        <div>
+                            <h3 className="displayH3">{this.props.displayItem.name}</h3>
+                            <ShowCarousel currentItem={this.props.displayItem}></ShowCarousel>
+                            {console.log(this.props.restaurant)}
+
+                        </div>
+                    </div>
+                    <div> <p className="displayPar">{this.props.displayItem.description}</p>
+                    </div>
+                    <div className="contChild">
+                        <Map location={this.props.displayItem.location}></Map>
+                       {console.log(this.props.displayItem.location)}
+                    </div>
                 </div>
             </div>
         )
